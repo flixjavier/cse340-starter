e4ffa3b695be53eb1b6a039a082d9d68c2d0ba60
flixjavier <flixjavier@gmail.com>
2025-09-13 10:20:56 -0600

    correct bugs

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const app = express()
 const static = require("./routes/static")
 
 const expressLayouts = require("express-ejs-layouts")
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
 
 /* ***********************
  * Routes
@@ -21,13 +28,6 @@ app.use(static)
 app.get('/',function(req,res){
   res.render('index', {title: 'Home'})
 })
-
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Local Server Information
